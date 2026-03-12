7a639e0b5bace0f7092befb3609196ebd7a1d2ac
Chad Fernandez <developer@chadfernandez.me>
2026-03-12 15:10:49 +0800

    update config and tooling

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import vike from "vike/plugin";
@@ -5,4 +6,15 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [vike(), react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "~": path.resolve(import.meta.dirname, "./src"),
+      "~/convex/*": path.resolve(import.meta.dirname, "./convex"),
+    },
+  },
+  server: {
+    watch: {
+      ignored: ["**/src/app/**"],
+    },
+  },
 });
